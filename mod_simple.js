5468359a50530e392c2e2ab63481784686b04245
Christophe LE GAL <legal@cervval.com>
2018-03-13 16:33:49 +0100

    Modif simple. Ajout htaccess

--- mod_simple.js
+++ mod_simple.js
@@ -1,1 +1,2 @@
 _grapheEnv={"S":{t:"sommet", name:"S", marques:{}}, "A":{t:"sommet", name:"A", marques:{}}, "B":{t:"sommet", name:"B", marques:{}}, "P":{t:"sommet", name:"P", marques:{}}};
+_grapheChange=true;
