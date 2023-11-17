d7ac7095e6324d7e091d8fd45f99afd1028a12a8
Christophe Le Gal <christophe-github@vinigou.org>
2023-11-17 16:58:19 +0100

    New modules (should have been added a few commits ago)

---
+++ modules.js
@@ -0,0 +1,103 @@
+import * as Env from "./environment.js";
+
+let modules = {};
+
+export function isLoaded(name){
+    return modules[name];
+}
+
+export function markLoaded(name){
+    modules[name]=true;
+}
+
+let _r=0;
+
+
+export function load(name, ln){
+    let url='Modules/mod_'+name+".json";
+    let req=new XMLHttpRequest();
+    req.responseType = 'json';
+    req.open('GET', url, false);
+    req.send(null);
+
+    if(req.status!=200) throw {error:"module", name:"Module introuvable", msg:`Module ${name} inexistant`, ln:ln};
+    if(!req.response) throw {error:"module", name:"Erreur dans le module", msg:`Impossible de charger module ${name}`, ln:ln};
+    let j=req.response;
+    let g=Env.Gr;
+    if(j.name) {
+        if(!Env.Graphes[j.name]) Env.addGraphe(j.name, ln);
+        g=Env.Graphes[j.name];
+    }
+    g.reset();
+    if(j.mode) g.mode=j.mode;
+    if(j.discover) g.discover=j.discover;
+    if(j.oriented) g.setOrient(j.oriented);
+    else g.setOrient(false);
+    if(j.names && j.pos && j.names.length!=j.pos.length){
+        throw {error:"interne", name:"Erreur dans le module", msg:`Nombre de noms et de position des sommets incohérents dans ${name}`, ln:ln};
+    }
+    let ns=0;
+    if(j.names) ns=j.names.length;
+    else if(j.pos) ns=j.pos.length;
+    g.sommets={};
+    g.arcs=[];
+    let soms=[];
+    for(let i=0; i<ns; i++){
+        let s=g.addNode(j.names?j.names[i]:"S"+(i+1), ln);
+        soms.push(s);
+        if(j.pos){
+            s.marques['x']={t:"number", val:j.pos[i][0]};
+            s.marques['y']={t:"number", val:j.pos[i][1]};
+        }
+    }
+    if(j.edge && j.edegs===undefined) j.edges=j.edge;
+    for(let p of j.edges){
+        let d=undefined;
+        let s1=soms[p[0]];
+        let s2=soms[p[1]];
+        if(p.length==3) d=p[2];
+        else if (j.pos && j.dist){
+            let x1=s1.marques.x;
+            let x2=s2.marques.x;
+            let y1=s1.marques.y;
+            let y2=s2.marques.y;
+            d=Math.sqrt((x1-x2)**2 + (y1-y2)**2);
+        }
+        let m={};
+        if(d!==undefined) m.val = {t:'number', val:d};
+        if(j.oriented) g.addArc(s1, s2, m);
+        else g.addArete(s1, s2, m);
+    }
+
+}
+
+function loadCb(json){
+    console.log(json);
+}
+
+
+function importGraphe(g,v){
+   for(let i=0; i<g[0].length; i++){
+      let x={t:"number", val:g[0][i][0]};
+      let y={t:"number", val:g[0][i][1]};
+      _grapheEnv["S"+i] = {t:"Sommet", name:"S"+i, marques:{x:x, y:y}};
+   }
+   for(let p of g[1]){
+      let s1=_grapheEnv["S"+p[0]];
+      let s2=_grapheEnv["S"+p[1]];
+      let d={t:"number", val:0};
+      if(p.length==3) d.val=p[2];
+      else{
+         let x1=s1.marques.x;
+         let x2=s2.marques.x;
+         let y1=s1.marques.x;
+         let y2=s2.marques.x;
+         d.val=Math.sqrt((x1-x2)**2 + (y1-y2)**2);
+      }
+      if (v=="noValues") _arcs.push({t:"Arete", i:s1, a:s2, marques:{}});
+      else _arcs.push({t:"Arete", i:s1, a:s2, marques:{val:d}});
+   }
+   _grapheChange=true;
+   _grapheMode="map";
+}
+
