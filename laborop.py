027b87a5c16c5a21de107d6d05f38f9b310c8a1e
Christophe Le Gal <christophe-git@vinigou.org>
2024-10-25 08:30:14 +0000

    Logging

--- laborop.py
+++ laborop.py
@@ -2,18 +2,23 @@ from flask import Flask, send_from_directory, redirect, request, session
 import os
 import requests
 import xmltodict
+import logging
+import datetime
 
 # configure flask app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 staticdir = os.path.join(basedir, 'static')
+logging.basicConfig(filename='/var/www/laborop/laborop.log', level=logging.DEBUG)
 
 @app.route("/")
 def root():
     if 'user' in session:
+        logging.debug(f"Root route called by {session['user']}. Redirecting to main")
         # Already connected. Just load main html file
         return redirect("static/main.html?fromroot")
     # Not connected. Redirect to CAS 
+    logging.debug(f"Root route called by unknown user. Redirecting to CAS")
     return redirect("https://cas.enib.fr/login?service=https://laborop.enib.fr/retourcas")
 
 @app.route("/retourcas", methods=['GET'])
@@ -22,7 +27,9 @@ def retourcas():
     # access to session['user'] and the kind
     try:
         ticket=request.args.get('ticket')
+        logging.debug(f"retourcas received ticket {ticket} presumably from CAS")
         r=requests.get(f"https://cas.enib.fr/serviceValidate?ticket={ticket}&service=https://laborop.enib.fr/retourcas")
+        logging.debug(f"CAS validation contains response: {'cas:serviceResponse' in r.content} and success {'cas:serviceResponse' in r.content and 'cas:authenticationSuccess' in r.content['cas:serviceResponse']}")
         xml=xmltodict.parse(r.content)['cas:serviceResponse']['cas:authenticationSuccess']
         session['user']=xml['cas:user']
         attrs=xml['cas:attributes']
@@ -32,7 +39,8 @@ def retourcas():
         session['cn']=attrs['cas:cn']
         session['uid']=attrs['cas:uid']
         session['role']=attrs['cas:eduPersonAffiliation']
-        return redirect("static/main.html#fromlogin")
+        logging.log(f"==Login== from «{session['user']}»≡«{session['uid']}»
+        return redirect("static/main.html?fromlogin")
     except:
         # Unless there was an error with CAS, in which case display a dumb error page
         return f"<html><body><h3 style='color:red'>Erreur d'authentification</h3><a href="/">Réessayer</a></body></html>"
