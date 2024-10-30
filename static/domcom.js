12d61d5dc57f8eaf545a7f0f62b5676302e4932b
Christophe Le Gal <christophe-git@vinigou.org>
2024-10-30 11:41:08 +0000

    Some really loosy \r support (basically, works only for «print("stage",i, end='\r', flush=True)»)

--- static/domcom.js
+++ static/domcom.js
@@ -5,7 +5,7 @@ import * as Env from "./environment.js";
 // String to be sent as stdout to console
 let _str='';
 let _strChange=false; // true iff _str has changed since last display of stdout
-let timeout=false;
+let lastPrint=0;
 
 // Just un log
 function myLog(msg){
@@ -16,28 +16,36 @@ function myLog(msg){
 // Redraw graph is needed (or if force)
 // Send stdout string to console
 export function regularCheck(force=false){
-    timeout=false;
     for(let i in Env.Graphes) Env.Graphes[i].redraw(force);
     
     if(_strChange || force){
-        flush();
+        actualFlush();
     }
 }
 
 function actualFlush(){
     if(!_strChange) return;
     _strChange=false;
+    lastPrint=Date.now();
     postMessage({print: _str});
+    console.log(Date.now());
 }
 
 export function flush(){
-    if(timeout) return;
+    let t=Date.now();
+    if(t-lastPrint<200) return;
     actualFlush();
-    timeout=true;
 }
 
 export function print(s){
     _str += s;
+    let lastR=_str.lastIndexOf('\r', _str.length-2);
+    if(lastR>=0){
+        let llR=_str.lastIndexOf('\r', lastR-1);
+        let llN=_str.lastIndexOf('\n', lastR-1);
+        if(llN>llR) llR=llN;
+        _str = _str.slice(0,llR+1)+_str.slice(lastR+1);
+    }
     _strChange=true;
     if(_str.length>11000) _str=_str.substring(_str.length-10000);
 }
