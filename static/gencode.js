74b4343cd2fb007924009ccef576e3fb165de423
Christophe Le Gal <christophe-github@vinigou.org>
2024-11-08 20:01:35 +0100

    Test

--- static/gencode.js
+++ static/gencode.js
@@ -1,6 +1,7 @@
 let code=[];
 
 let labels={};
+// Generate code for decorated tree
 export function gencode(tree){
     
 }
--- static/semantic.js
+++ static/semantic.js
@@ -1,2 +1,3 @@
+// Decorate the tree (points variable to their environmen)
 export function semantic(tree){
 }
